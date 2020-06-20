1f2476395c06b46a529fbb6a73f038f38f55f04f
Carl Redding <credding07@gmail.com>
2020-06-19 23:04:35 -0400

    queue array established and all tests passing. just have to remember to remove from the end of the array and not the beginning.

--- queue/queue.py
+++ queue/queue.py
@@ -71,34 +71,60 @@ class LinkedList:
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
 
-1. Implement the Queue class using an array as the underlying storage structure.
-   Make sure the Queue tests pass.
-2. Re-implement the Queue class, this time using the linked list implementation
-   as the underlying storage structure.
-   Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+#Using an array
+'''
+1. Implement the Queue class using an array as the underlying storage structure.
+   Make sure the Queue tests pass.
+
+'''
+#Using a linked list
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
-    
+        self.storage = []
+
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
-        #add new value to the tail of our list
         self.size += 1
-        self.storage.add_to_tail(value)
+        self.storage.insert(0, value)
 
     def dequeue(self):
-        if self.size == 0:
+        if len(self.storage) == 0:
             return None
-        #remove the value from the head of our list
         self.size -= 1
-        value = self.storage.remove_head()
-        return value
+        #this is removing the LAST element in storage
+        node = self.storage.pop(-1)
+        return node
+'''
+2. Re-implement the Queue class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Queue tests pass.
+'''
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         #add new value to the tail of our list
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+
+#     def dequeue(self):
+#         if self.size == 0:
+#             return None
+#         #remove the value from the head of our list
+#         self.size -= 1
+#         value = self.storage.remove_head()
+#         return value
